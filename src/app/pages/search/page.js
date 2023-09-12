d41c665e88e7ddcf4da0de2456eb10d8a11d93a1
harpreet-phiteck <harpreet.phiteck@gmail.com>
2023-09-12 18:02:35 +0530

    add search page

---
+++ src/app/pages/search/page.js
@@ -0,0 +1,141 @@
+import './result.css';
+export default function Search(){
+    return(
+        <main class="main_wrapper">
+        <div class="container">
+            <section class="main_result_sec">
+                <div class="result_header">
+                    <h1>Italy</h1>
+                    <div class="search_div">
+                        <div class="search_heading">
+                            <span><img src="/search.png" alt="Search..."/> Any special details?</span>
+                        </div>
+                        <div class="search_submit">
+                            <img src="/MagicWand.png" alt="Magic Wand"/>
+                        </div>
+                    </div>
+                    <div class="save_share_cont">
+                        <img src="/Save and share (1).png" alt=""/>
+                        <img src="/Save and share (2).png" alt=""/>
+                    </div>
+                </div>
+                <div class="result_detail_wrapper">
+                    <div class="result_detail_sec">
+                        <div class="result_header_sec">
+                            <h1>Day 1</h1>
+                            <img src="/dot.png" alt="Dot"/>
+                            <div class="result_date">
+                                <p><img src="/Calender3.png" alt="Calendar"/> 07.08.23</p>
+                            </div>
+                        </div>
+                        <div class="morning_container">
+                            <div class="morning_heading">
+                                <h2>Morning</h2>
+                            </div>
+                            <div class="morning_description">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea voluptate atque suscipit non voluptatum consequatur inventore omnis, laudantium soluta debitis asperiores similique reprehenderit neque recusandae enim aut! Necessitatibus, ut impedit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet, assumenda, a distinctio praesentium nihil esse voluptas consectetur ea totam deleniti eveniet!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea voluptate atque suscipit non voluptatum consequatur inventore omnis.
+                            </div>
+                            <div class="morning_img_cont">
+                                <div>
+                                    <img src="/morning.png" class="place_img" width="100%" alt=""/>
+                                </div>
+                                <div class="place_review_cont">
+                                    <div>
+                                        <h3>Name of the place<span style={{color: '#798685', fontWeight: '100', paddingInline: '8px', fontSize: '16px'}}>(557 reviews)</span></h3>
+                                        <div style={{paddingTop: '5px', display: 'flex', alignItems: 'center'}}>
+                                            <img src="/Star 3.png" alt=""/>
+                                            <span style={{color: '#00AEA4', paddingLeft: '8px'}}>5.0</span>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <button>Book now</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <img src="/Vector 8.png"  class="border_img" alt=""/>
+                        </div>
+                        <div class="morning_container">
+                            <div class="morning_heading">
+                                <h2 class="afternoon">Afternoon</h2>
+                            </div>
+                            <div class="morning_description">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea voluptate atque suscipit non voluptatum consequatur inventore omnis, laudantium soluta debitis asperiores similique reprehenderit neque recusandae enim aut! Necessitatibus, ut impedit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet, assumenda, a distinctio praesentium nihil esse voluptas consectetur ea totam deleniti eveniet!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea voluptate atque suscipit non voluptatum consequatur inventore omnis.
+                            </div>
+                            <div class="morning_img_cont">
+                                <div>
+                                    <img src="/afternoon.png" class="place_img" width="100%" alt=""/>
+                                </div>
+                                <div class="place_review_cont">
+                                    <div>
+                                        <h3>Name of the place<span style={{color: '#798685', fontWeight: '100', paddingInline: '8px', fontSize: '16px'}}>(557 reviews)</span></h3>
+                                        <div style={{paddingTop: '5px', display: 'flex', alignItems: 'center'}}>
+                                            <img src="/Star 3.png" alt=""/>
+                                            <span style={{color: '#00AEA4', paddingLeft: '8px'}}>5.0</span>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <button>Book now</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <img src="/Vector 8.png"  class="border_img" alt=""/>
+                        </div>
+                        <div class="morning_container">
+                            <div class="morning_heading">
+                                <h2 class="evening">Evening</h2>
+                            </div>
+                            <div class="morning_description">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea voluptate atque suscipit non voluptatum consequatur inventore omnis, laudantium soluta debitis asperiores similique reprehenderit neque recusandae enim aut! Necessitatibus, ut impedit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet, assumenda, a distinctio praesentium nihil esse voluptas consectetur ea totam deleniti eveniet!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea voluptate atque suscipit non voluptatum consequatur inventore omnis.
+                            </div>
+                            <div class="morning_img_cont">
+                                <div>
+                                    <img src="/evening.png" class="place_img" width="100%" alt=""/>
+                                </div>
+                                <div class="place_review_cont">
+                                    <div>
+                                        <h3>Name of the place<span style={{color: '#798685', fontWeight: '100', paddingInline: '8px', fontSize: '16px'}}>(557 reviews)</span></h3>
+                                        <div style={{paddingTop: '5px', display: 'flex', alignItems: 'center'}}>
+                                            <img src="/Star 3.png" alt=""/>
+                                            <span style={{color: '#00AEA4', paddingLeft: '8px'}}>5.0</span>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <button>Book now</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <img src="/Vector 8.png"  class="border_img" alt=""/>
+                        </div>
+                        <div class="morning_container">
+                            <div class="morning_heading">
+                                <h2 class="hotels">Hotels</h2>
+                            </div>
+                            <div class="morning_description">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea voluptate atque suscipit non voluptatum consequatur inventore omnis, laudantium soluta debitis asperiores similique reprehenderit neque recusandae enim aut! Necessitatibus, ut impedit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet, assumenda, a distinctio praesentium nihil esse voluptas consectetur ea totam deleniti eveniet!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea voluptate atque suscipit non voluptatum consequatur inventore omnis.
+                            </div>
+                            <div class="morning_img_cont">
+                                <div>
+                                    <img src="/hotels.png" class="place_img" width="100%" alt=""/>
+                                </div>
+                                <div class="place_review_cont">
+                                    <div>
+                                        <h3>Name of the place<span style={{color: '#798685', fontWeight: '100', paddingInline: '8px', fontSize: '16px'}}>(557 reviews)</span></h3>
+                                        <div style={{paddingTop: '5px', display: 'flex', alignItems: 'center'}}>
+                                            <img src="/Star 3.png" alt=""/>
+                                            <span style={{color: '#00AEA4', paddingLeft: '8px'}}>5.0</span>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <button>Book now</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <img src="/Vector 8.png"  class="border_img" alt=""/>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </main>
+    )
+}
